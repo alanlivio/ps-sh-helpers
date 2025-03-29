6a71995098828395e1764de958a337eae3567757
Alan Guedes <alanlivio@outlook.com>
2025-03-29 16:42:11 +0000

    refact init

--- init.sh
+++ init.sh
@@ -1,16 +1,16 @@
+# -- any OS --
+
 HELPERS_DIR="$(dirname "${BASH_SOURCE[0]}")"
-
-# -- bash basic --
-
 function log_error() { echo -e "\033[00;31m-- $* \033[00m"; }
 function log_msg() { echo -e "\033[00;33m-- $* \033[00m"; }
-# TODO: convert usefull from ps1 to functions
 alias bashrc_reload='source $HOME/.bashrc'
 alias folder_count_files='find . -maxdepth 1 -type f | wc -l'
 alias folder_count_files_recusive='find . -maxdepth 1 -type f | wc -l'
 alias folder_list_sorted_by_size='du -ahd 1 | sort -h'
 alias folder_find_file_with_crlf='find . -not -type d -exec file "{}" ";" | grep CRLF'
 alias passwd_generate='echo $(tr -dc "A-Za-z0-9!?%=" < /dev/urandom | head -c 12)'
+
+# -- _ helpers to create aliases --
 
 function _ps_call() {
     powershell -command "& { . $(wslpath -w $HELPERS_DIR/init.ps1); $* }"
