97a8685b947b31529fdeab42ca4afe548781041b
Alan Guedes <alanlivio@outlook.com>
2025-04-14 14:23:19 +0100

    add any.ps1 and any.bash

---
+++ os/any.ps1
@@ -0,0 +1,20 @@
+# -- basic --
+
+function log_msg() { Write-Host -ForegroundColor DarkYellow "--" ($args -join " ") }
+function log_error() { Write-Host -ForegroundColor DarkRed "--" ($args -join " ") }
+function passwd_generate { -join (1..12 | ForEach-Object { 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!?' | Get-Random }) }
+
+# -- ps --
+
+function ps_profile_reload() {
+    # TODO: this does not work properly
+    . $PROFILE.CurrentUserAllHosts
+}
+
+function ps_is_running_as_sudo { 
+    ([System.Security.Principal.WindowsPrincipal] [System.Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
+function ps_func_show($name) {
+    Get-Content Function:\$name
+}
