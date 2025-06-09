66eced4461f76c15276da23eda25dff569326671
Alan Guedes <alanlivio@outlook.com>
2025-06-09 19:39:50 +0100

    inti.sh: disable remove ps_def_funcs_from_ps1_file

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ source "$HELPERS_DIR/os/any.bash"
 
 if [[ $OSTYPE == msys* || -n $WSL_DISTRO_NAME ]]; then
     source "$HELPERS_DIR/os/win.bash"
-    ps_def_funcs_from_ps1_file "$HELPERS_DIR/os/win.ps1"
+    # ps_def_funcs_from_ps1_file "$HELPERS_DIR/os/win.ps1"
 fi
 if [[ $OSTYPE == linux* ]]; then
     source "$HELPERS_DIR/os/ubu.bash"
