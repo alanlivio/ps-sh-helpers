2ec223a0b3516447ccd89156e0d2252df71a1532
Alan Guedes <alanlivio@outlook.com>
2025-07-02 13:56:18 +0100

    git.ps1: add git_clone_to_dir at

--- programs/git.ps1
+++ programs/git.ps1
@@ -13,3 +13,27 @@ function git_gitignore_types_add {
 function git_gitignore_types_add_vscode {
     (Invoke-WebRequest -Uri "https://www.toptal.com/developers/gitignore/api/visualstudiocode" -UseBasicParsing).Content
 }
+
+function git_clone_to_dir {
+    param(
+        [Parameter(Mandatory = $true, HelpMessage = "Usage: git_clone_to_dir <url> <dir> [<name>] [<email>]. If <name> is empty, the <url> basename is used.")]
+        [string]$url,
+        [Parameter(Mandatory = $true)]
+        [string]$dir,
+        [string]$name,
+        [string]$email
+    )
+    if ([string]::IsNullOrEmpty($name)) {
+        $dir = Join-Path $dir (Split-Path $url -Leaf)
+    } else {
+        $dir = Join-Path $dir $name
+    }
+    if (-not (Test-Path $dir)) {
+        git clone $url $dir
+    }
+    if (-not ([string]::IsNullOrEmpty($email))) {
+        Push-Location $dir
+        git config user.email "$email"
+        Pop-Location
+    }
+}
