a62f9285b90a65180831ab74aacd91cb5103ffe9
Alan Guedes <alanlivio@outlook.com>
2025-07-15 12:12:51 +0100

    add ssh.ps1

---
+++ programs/ssh.ps1
@@ -0,0 +1,10 @@
+function ssh_send_authorized_public_key {
+    param([Parameter(Mandatory = $true)][string]$UserServer)
+    $publicKeyPath = Join-Path $HOME ".ssh\id_rsa.pub"
+    (Get-Content $publicKeyPath -Raw) -replace "`r`n", "`n"  | ssh "$UserServer" "sh -c 'cat - >> ~/.ssh/authorized_keys'"
+}
+function ssh_send_private_key {
+    param([Parameter(Mandatory = $true)][string]$UserServer)
+    $privateKeyPath = Join-Path $HOME ".ssh\id_rsa"
+    (Get-Content $privateKeyPath -Raw) -replace "`r`n", "`n" | ssh "$UserServer" "sh -c 'cat - > ~/.ssh/id_rsa;chmod 600 ~/.ssh/id_rsa'"
+}
