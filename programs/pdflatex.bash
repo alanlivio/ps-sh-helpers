52b398b638d14222855d1501262cec5ee604c8da
Alan Guedes <a.guedes@reading.ac.uk>
2025-09-04 20:29:26 +0100

    add latex_word_count

--- programs/pdflatex.bash
+++ programs/pdflatex.bash
@@ -1,3 +1,8 @@
 function latex_clean() {
     rm -rf comment.cut ./*.aux ./*.dbx ./*.bbx ./*.cbx ./*.dvi ./*.log ./*.lox ./*.out ./*.lol ./*.pdf ./*.synctex.gz ./_minted-* ./*.bbl ./*.blg ./*.lot ./*.lof ./*.toc ./*.lol ./*.fdb_latexmk ./*.fls ./*.bcf ./*.aux ./*.fls ./*.fdb_latexmk ./*.log
 }
+
+function latex_word_count(){
+    : ${1?"Usage: ${FUNCNAME[0]} <file>"}
+    texcount -inc -sum $1 | awk -F': ' '/^Sum count:/ {print $2}'
+}
