ab2d9ce2ab078fa02361f65b6f37d61447e8470e
Alan Guedes <alanlivio@outlook.com>
2025-09-17 19:39:26 +0100

    add pdflatex.ps1

--- programs/pdflatex.bash
+++ programs/pdflatex.bash
@@ -1,8 +1,8 @@
 function latex_clean() {
-    rm -rf comment.cut ./*.aux ./*.dbx ./*.bbx ./*.cbx ./*.dvi ./*.log ./*.lox ./*.out ./*.lol ./*.pdf ./*.synctex.gz ./_minted-* ./*.bbl ./*.blg ./*.lot ./*.lof ./*.toc ./*.lol ./*.fdb_latexmk ./*.fls ./*.bcf ./*.aux ./*.fls ./*.fdb_latexmk ./*.log
+    rm -f comment.cut ./*.aux ./*.dbx ./*.bbx ./*.cbx ./*.dvi ./*.log ./*.lox ./*.out ./*.lol ./*.pdf ./*.synctex.gz ./_minted-* ./*.bbl ./*.blg ./*.lot ./*.lof ./*.toc ./*.lol ./*.fdb_latexmk ./*.fls ./*.bcf ./*.aux ./*.fls ./*.fdb_latexmk ./*.log
 }
 
 function latex_word_count(){
-    : ${1?"Usage: ${FUNCNAME[0]} <file>"}
+    : ${1?"Usage: ${FUNCNAME[0]} <file.tex>"}
     texcount -inc -sum $1 | awk -F': ' '/^Sum count:/ {print $2}'
 }
