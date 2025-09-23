5777489094a3c5f653b85bb02c5c22581a376c07
Alan Guedes <alanlivio@outlook.com>
2025-09-23 16:39:09 +0100

    update ssh helpers

--- programs/ssh.bash
+++ programs/ssh.bash
@@ -1,9 +1,9 @@
-function ssh_send_to_server_authorized_pub_key() {
+function ssh_send_to_pub_key_to_remote() {
     : ${1?"Usage: ${FUNCNAME[0]} <user@server>"}
     ssh "$1" sh -c 'cat - >> ~/.ssh/authorized_keys' <$HOME/.ssh/id_rsa.pub
 }
 
-function ssh_send_to_server_priv_key() {
+function ssh_send_priv_key_to_remote() {
     : ${1?"Usage: ${FUNCNAME[0]} <user@server>"}
     ssh "$1" sh -c 'cat - > ~/.ssh/id_rsa;chmod 600 $HOME/.ssh/id_rsa' <$HOME/.ssh/id_rsa
 }
--- programs/ssh.ps1
+++ programs/ssh.ps1
@@ -1,11 +1,13 @@
-function ssh_send_authorized_public_key {
-    param([Parameter(Mandatory = $true)][string]$UserServer)
+function ssh_send_to_pub_key_to_remote {
+    if ($PSBoundParameters.Keys.Count -lt 1) { log_error "Usage: ssh_send_to_pub_key_to_remote user@host"; }
+    param([Parameter(Mandatory = $true)][string]$server)
     $publicKeyPath = Join-Path $env:userprofile ".ssh\id_rsa.pub"
-    (Get-Content $publicKeyPath -Raw) -replace "`r`n", "`n"  | ssh "$UserServer" "sh -c 'cat - >> ~/.ssh/authorized_keys'"
+    (Get-Content $publicKeyPath -Raw) -replace "`r`n", "`n"  | ssh "$server" "sh -c 'cat - >> ~/.ssh/authorized_keys'"
 }
 
-function ssh_send_private_key {
-    param([Parameter(Mandatory = $true)][string]$UserServer)
+function ssh_send_priv_key_to_remote {
+    if ($PSBoundParameters.Keys.Count -lt 1) { log_error "Usage: ssh_send_priv_key_to_remote user@host"; }
+    param([Parameter(Mandatory = $true)][string]$server)
     $privateKeyPath = Join-Path $env:userprofile ".ssh\id_rsa"
-    (Get-Content $privateKeyPath -Raw) -replace "`r`n", "`n" | ssh "$UserServer" "sh -c 'cat - > ~/.ssh/id_rsa;chmod 600 ~/.ssh/id_rsa'"
+    (Get-Content $privateKeyPath -Raw) -replace "`r`n", "`n" | ssh "$server" "sh -c 'cat - > ~/.ssh/id_rsa;chmod 600 ~/.ssh/id_rsa'"
 }
