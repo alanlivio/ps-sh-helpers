56b8aa4979d5ead5fc05bc360bf14512a01fe096
Alan Guedes <a.guedes@reading.ac.uk>
2025-09-28 20:35:11 +0100

    add profile_add_helpers

--- init.ps1
+++ init.ps1
@@ -1,5 +1,17 @@
 $HELPERS_DIR = Split-Path -Parent $MyInvocation.MyCommand.Path
 
+function profile_add_helpers { 
+    $sourceLine = '. "$HELPERS_DIR/init.ps1"'
+    if (-not (Test-Path $profile)) {
+        $folder = [System.IO.Path]::GetDirectoryName($profile)
+        New-Item -Path $folder -ItemType Directory | Out-Null
+        New-Item $profile -type file | Out-Null
+    } elseif (-not (Select-String -Path $profile -SimpleMatch $sourceLine -Quiet)) {
+        Add-Content $profile $sourceLine
+    }
+    log_msg "init.ps1 added to $profile"
+}
+
 # -- load os/<name>.ps1 files -- 
 
 . "$HELPERS_DIR/os/any.ps1"
